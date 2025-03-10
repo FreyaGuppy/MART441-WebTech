b5ab61c90987c1ba694a5847423aba370a79f6cf
FreyaGuppy <freya.gooday@umontana.edu>
2025-03-09 22:47:09 -0600

    add homework 8

---
+++ HW8/main.js
@@ -0,0 +1,64 @@
+var myPictureBookArray = new Array();
+var currentIndex = 0; // Track the current index
+
+class PictureBook {
+    constructor(imagePath, caption) {
+        this.imagePath = imagePath;
+        this.caption = caption;
+    }
+
+    toString() {
+        return this.imagePath + "<br>" + this.caption;
+    }
+}
+
+// this function is called in the body of the HTML page so that the objects are created and added to the array
+function initializeArray() {
+    myPictureBookArray.push(new PictureBook("images/bigSmile.gif", "With uncontrollable thoughts come uncontrollable jealousies."));
+    myPictureBookArray.push(new PictureBook("images/happy.webp", "Global decay hasn't even begun."));
+    myPictureBookArray.push(new PictureBook("images/puter.jpeg", "Let people know that medieval torture is actually really cool."));
+    myPictureBookArray.push(new PictureBook("images/sadEyes.jpg", "Becoming amazing at burning things demands passion."));
+    myPictureBookArray.push(new PictureBook("images/sitPretty.jpeg", "Show your support for people whom you don't like."));
+    myPictureBookArray.push(new PictureBook("images/sob.jpg", "Feel lonely. Or don't."));
+    myPictureBookArray.push(new PictureBook("images/strangle.jpg", "Quit smiling and keep yourself prioritized."));
+    myPictureBookArray.push(new PictureBook("images/sunglasses.webp", "Magick. Something that goes all the way to the top?"));
+    myPictureBookArray.push(new PictureBook("images/sweaty.webp", "If you want inspirational quotes you will have to learn to accept evil."));
+    myPictureBookArray.push(new PictureBook("images/today.jpeg", "Abuse your position and bamboozle people."));
+}
+
+function accessInformation() {
+    // Get the next object from the array
+    var selectedPictureBook = myPictureBookArray[currentIndex];
+    var imageContainer = $("#imageContainer");
+    var titleContainer = $("#title");
+    var fadeDuration = 1500;
+    var moveDistance = 300;
+
+    var currentImage = imageContainer.children("img");
+    var currentTitle = titleContainer.children("i"); // Target the text inside <i> tags
+
+    // Ensure the image has a relative position for animation
+    if (!currentImage.length) {
+        imageContainer.html(`<img src="${selectedPictureBook.imagePath}" style="width: 400px; position: relative;">`);
+        titleContainer.html(`<i style="position: relative;">${selectedPictureBook.caption}</i>`);
+        return;
+    }
+
+    // Animate both the image and the title moving to the right and fading out
+    currentImage.animate({ left: `+=${moveDistance}px`, opacity: 0 }, 200);
+    currentTitle.animate({ left: `+=${moveDistance}px`, opacity: 0 }, 500, function () {
+        // After animation, replace the content
+        imageContainer.html(`<img src="${selectedPictureBook.imagePath}" style="width: 400px; position: relative; left: -${moveDistance}px; opacity: 0;">`);
+        titleContainer.html(`<i style="position: relative; left: -${moveDistance}px; opacity: 0;">${selectedPictureBook.caption}</i>`);
+
+        // Fade in and move the new image and title back to position
+        var newImage = imageContainer.children("img");
+        var newTitle = titleContainer.children("i");
+        
+        newImage.animate({ left: "0px", opacity: 1 }, 200);
+        newTitle.animate({ left: "0px", opacity: 1 }, 500);
+    });
+
+    // Move to the next index, loop back to 0 if at the end
+    currentIndex = (currentIndex + 1) % myPictureBookArray.length;
+}
