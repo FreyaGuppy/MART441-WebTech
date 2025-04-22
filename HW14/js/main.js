7bbac3cc8938355523782d5ff2e7c233dde67dbd
FreyaGuppy <freya.gooday@umontana.edu>
2025-04-21 20:38:13 -0600

    update HW14

--- HW14/js/main.js
+++ HW14/js/main.js
@@ -7,12 +7,12 @@ function createBox() {
   // create a box
   var geometry = new THREE.BoxGeometry();
   var material = new THREE.MeshBasicMaterial({
-    color: 0x324ca8
+    color: 0x59472e
   });
   cube = new THREE.Mesh(geometry, material);
-  cube.position.set(50, 0, 0);
+  cube.position.set(0, 0, -40);
   scene.add(cube);
-  cube.scale.x = 15; // SCALE
+  cube.scale.x = 70; // SCALE
   cube.scale.y = 15; // SCALE
   cube.scale.z = 15; // SCALE
 
@@ -23,8 +23,8 @@ function createBox() {
 // animate the first box
 function animate() {
   requestAnimationFrame(animate);
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
+  cube.rotation.x += 0.1;
+  cube.rotation.y += 0;
   createBox2();
   renderer.render(scene, camera);
 
@@ -36,14 +36,14 @@ function createBox2() {
   // create a box
   var geometry = new THREE.BoxGeometry();
   var material = new THREE.MeshBasicMaterial({
-    color: 0x1234ee
+    color: 0xfff126
   });
   cube2 = new THREE.Mesh(geometry, material);
-  cube2.position.set(2, 0);
+  cube2.position.set(0, 0, 5);
   cube.add(cube2);
-  cube2.scale.x = .5; // SCALE
-  cube2.scale.y = .5; // SCALE
-  cube2.scale.z = .5; // SCALE
+  cube2.scale.x = .4; // SCALE
+  cube2.scale.y = .1; // SCALE
+  cube2.scale.z = .1; // SCALE
 
   animate2();
 }
@@ -63,7 +63,7 @@ function animate2() {
 
 function getScene() {
   var scene = new THREE.Scene();
-  scene.background = new THREE.Color(0xaaaaaa);
+  scene.background = new THREE.Color(0x49a31f);
   return scene;
 }
 
